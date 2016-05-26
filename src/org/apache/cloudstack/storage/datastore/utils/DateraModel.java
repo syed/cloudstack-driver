6c3294b8761f85569e3e10780d899db578b70c40
bijaytiwari <bijay.tiwari@maplelabs.com>
2016-05-26 17:59:39 +0530

    moved models

--- src/org/apache/cloudstack/storage/datastore/utils/DateraModel.java
+++ src/org/apache/cloudstack/storage/datastore/utils/DateraModel.java
@@ -2,9 +2,182 @@ package org.apache.cloudstack.storage.datastore.utils;
 
 import java.util.List;
 
+import com.google.gson.annotations.SerializedName;
+
 public class DateraModel {
 
-     public static class InitiatorGroup
+    public static final String defaultStorage="storage-1"; 
+    public static final String defaultVolume="volume-1"; 
+    private class LoginModel
+     {
+      public String name;
+      public String password;
+     }
+
+     public static class ACLPolicyModel
+     {
+      public List<String> initiators; // e.g. "/initiators/iqn.1993-08.org.debian:01:6e3ca7bd74e1"
+      @SerializedName("initiator_groups")
+      public List<String> initiatorGroups; // e.g. "/initiator_groups/cluster2_initiator_group"
+
+      public ACLPolicyModel(List<String> paramInitiators, List<String> paramInitiatorGroups)
+      {
+       initiators = paramInitiators;
+       initiatorGroups = paramInitiatorGroups;
+      }
+     }
+     public static class AppModel
+     {
+      public String name;
+      @SerializedName("access_control_mode")
+      public String accessControlMode;
+      @SerializedName("storage_instances")
+      public StorageInstanceModel storageInstances;
+
+      public AppModel(String appInstanceName, String accessControlMode, StorageInstanceModel inst)
+      {
+       name = appInstanceName;
+       accessControlMode = accessControlMode;
+       storageInstances = inst;
+      }
+     }
+
+     public class StorageInstanceModel
+     {
+      @SerializedName(defaultStorage)
+      public StorageModel storage1;
+
+      public StorageInstanceModel(StorageModel st)
+      {
+       storage1 = st;
+      }
+     }
+
+     public static class StorageModel
+     {
+      public String name = defaultStorage;
+      public VolumeInstanceModel volumes;
+      @SerializedName("acl_policy")
+      public ACLPolicyModel aclPolicy;
+      @SerializedName("ip_pool")
+      public String ipPool;
+      public StorageModel(String networkPoolName, VolumeInstanceModel vols, ACLPolicyModel policy)
+      {
+       volumes = vols;
+       aclPolicy = policy;
+       ipPool = networkPoolName;
+      }
+     }
+
+     public static class VolumeInstanceModel
+     {
+      @SerializedName(defaultVolume)
+      public VolumeModel volume1;
+
+      public VolumeInstanceModel(VolumeModel vol)
+      {
+       volume1 = vol;
+      }
+     }
+     public static class VolumeModel {
+
+      public String name = defaultVolume;
+      public int size;
+      @SerializedName("replica_count")
+      public int replicaCount;
+      //snapshot policies
+
+      public VolumeModel(String volName,int volSize, int volReplicaSize)
+      {
+       name =  volName;
+       size = volSize;
+       replicaCount = volReplicaSize;
+      }
+     }
+
+     public static class StorageResponse
+     {
+      public class AccessControl
+      {
+       public List<String> ips;
+       public String iqn;
+      }
+
+      public AccessControl access;
+
+     }
+     public static class AdminPreviledge
+     {
+       @SerializedName("admin_state")
+       public String adminState;
+
+       public AdminPreviledge(String paramAdminState)
+       {
+        adminState = paramAdminState;
+       }
+     }
+
+     public static class GenericResponse
+     {
+      public String name;
+      public String id;
+     }
+
+     public static class VolumeResize
+     {
+         public int size;
+         public VolumeResize(int sz)
+         {
+           size = sz;
+         }
+     }
+
+     public static class AppModelEx
+     {
+         public String name;
+
+         public AppModelEx(String appName)
+         {
+           name = appName;
+         }
+     }
+
+     public static class StorageModelEx
+     {
+         public String name;
+         @SerializedName("ip_pool")
+         public String ipPool;
+
+         public StorageModelEx(String storageName, String ipPool)
+         {
+           name = storageName;
+           this.ipPool = ipPool;
+         }
+     }
+     public static class InitiatorModel
+     {
+        public String id;
+        public String name;
+       public InitiatorModel(String label, String iqn)
+       {
+         name = label;
+         id = iqn;
+       }
+     }
+     public static class DateraError
+     {
+        public String name;
+        public int code;
+        public int http;
+        public String message;
+        @SerializedName("api_req_id")
+        public int apiReqId;
+        @SerializedName("storage_node_uuid")
+        public String storageNodeUuid;
+        public String ts;
+     }
+    
+    public static class InitiatorGroup
      {
       public String name;
       public List<String> members = null;
