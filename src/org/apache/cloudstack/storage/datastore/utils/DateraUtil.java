f7d501fdeb6a8af72af4c46f2dc2230849835073
Titu Nahak <titu.hru1@gmail.com>
2016-06-16 05:08:15 -0700

    added new testcases for DateraUtil

--- src/org/apache/cloudstack/storage/datastore/utils/DateraUtil.java
+++ src/org/apache/cloudstack/storage/datastore/utils/DateraUtil.java
@@ -16,7 +16,6 @@
 // under the License.
 package org.apache.cloudstack.storage.datastore.utils;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -26,7 +25,7 @@ import org.apache.cloudstack.storage.datastore.utils.DateraUtil.DateraMetaData;
 
 import com.cloud.host.Host;
 import com.cloud.host.HostVO;
-import com.cloud.utils.exception.CloudRuntimeException;
+
 
 public class DateraUtil {
     public static class DateraMetaData {
@@ -115,20 +114,6 @@ public class DateraUtil {
     private static final String DEFAULT_NETWORK_POOL_NAME = "default";
 
 
-    private static String getVip(String keyToMatch, String url) {
-        String delimiter = ":";
-
-        String storageVip = getValue(keyToMatch, url);
-
-        int index = storageVip.indexOf(delimiter);
-
-        if (index != -1) {
-            return storageVip.substring(0, index);
-        }
-
-        return storageVip;
-    }
-
     public static boolean hostsSupport_iScsi(List<HostVO> hosts) {
         if (hosts == null || hosts.size() == 0) {
             return false;
@@ -143,139 +128,6 @@ public class DateraUtil {
         return true;
     }
 
-    public static String[] getNewHostIqns(String[] currentIqns, String[] newIqns) {
-        List<String> lstIqns = new ArrayList<String>();
-
-        if (currentIqns != null) {
-            for (String currentIqn : currentIqns) {
-                lstIqns.add(currentIqn);
-            }
-        }
-
-        if (newIqns != null) {
-            for (String newIqn : newIqns) {
-                if (!lstIqns.contains(newIqn)) {
-                    lstIqns.add(newIqn);
-                }
-            }
-        }
-
-        return lstIqns.toArray(new String[0]);
-    }
-
-    public static long[] getNewVolumeIds(long[] volumeIds, long volumeIdToAddOrRemove, boolean add) {
-        if (add) {
-            return getNewVolumeIdsAdd(volumeIds, volumeIdToAddOrRemove);
-        }
-
-        return getNewVolumeIdsRemove(volumeIds, volumeIdToAddOrRemove);
-    }
-
-    private static long[] getNewVolumeIdsAdd(long[] volumeIds, long volumeIdToAdd) {
-        List<Long> lstVolumeIds = new ArrayList<Long>();
-
-        if (volumeIds != null) {
-            for (long volumeId : volumeIds) {
-                lstVolumeIds.add(volumeId);
-            }
-        }
-
-        if (lstVolumeIds.contains(volumeIdToAdd)) {
-            return volumeIds;
-        }
-
-        lstVolumeIds.add(volumeIdToAdd);
-
-        return convertArray(lstVolumeIds);
-    }
-
-    private static long[] getNewVolumeIdsRemove(long[] volumeIds, long volumeIdToRemove) {
-        List<Long> lstVolumeIds = new ArrayList<Long>();
-
-        if (volumeIds != null) {
-            for (long volumeId : volumeIds) {
-                lstVolumeIds.add(volumeId);
-            }
-        }
-
-        lstVolumeIds.remove(volumeIdToRemove);
-
-        return convertArray(lstVolumeIds);
-    }
-
-    private static long[] convertArray(List<Long> items) {
-        if (items == null) {
-            return new long[0];
-        }
-
-        long[] outArray = new long[items.size()];
-
-        for (int i = 0; i < items.size(); i++) {
-            Long value = items.get(i);
-
-            outArray[i] = value;
-        }
-
-        return outArray;
-    }
-
-    public static String[] getIqnsFromHosts(List<? extends Host> hosts) {
-        if (hosts == null || hosts.size() == 0) {
-            throw new CloudRuntimeException("There do not appear to be any hosts in this cluster.");
-        }
-
-        List<String> lstIqns = new ArrayList<String>();
-
-        for (Host host : hosts) {
-            lstIqns.add(host.getStorageUrl());
-        }
-
-        return lstIqns.toArray(new String[0]);
-    }
-
-    private static List<String> getStringArrayAsLowerCaseStringList(String[] aString) {
-        List<String> lstLowerCaseString = new ArrayList<String>();
-
-        if (aString != null) {
-            for (String str : aString) {
-                if (str != null) {
-                    lstLowerCaseString.add(str.toLowerCase());
-                }
-            }
-        }
-
-        return lstLowerCaseString;
-    }
-
-    private static boolean isSuccess(int iCode) {
-        return iCode >= 200 && iCode < 300;
-    }
-
-    public static String getModifiedUrl(String originalUrl) {
-        StringBuilder sb = new StringBuilder();
-
-        String delimiter = ";";
-
-        StringTokenizer st = new StringTokenizer(originalUrl, delimiter);
-
-        while (st.hasMoreElements()) {
-            String token = st.nextElement().toString().toUpperCase();
-
-            if (token.startsWith(DateraUtil.MANAGEMENT_IP.toUpperCase()) ) {
-                sb.append(token).append(delimiter);
-            }
-        }
-
-        String modifiedUrl = sb.toString();
-        int lastIndexOf = modifiedUrl.lastIndexOf(delimiter);
-
-        if (lastIndexOf == (modifiedUrl.length() - delimiter.length())) {
-            return modifiedUrl.substring(0, lastIndexOf);
-        }
-
-        return modifiedUrl;
-    }
-
     public static String getManagementIP(String url) {
         return getIP(DateraUtil.MANAGEMENT_IP, url);
     }
@@ -283,10 +135,6 @@ public class DateraUtil {
 
     public static int getManagementPort(String url) {
         return getPort(DateraUtil.MANAGEMENT_PORT, url);
-    }
-
-    public static String getStorageVip(String url) {
-        return getVip(DateraUtil.STORAGE_VIP, url);
     }
 
     private static String getIP(String keyToMatch, String url) {
@@ -365,51 +213,11 @@ public class DateraUtil {
         return value;
     }
 
-    private static Long getLongValue(String key,String url)
-    {
-        Long value;
-        try
-        {
-            value = Long.parseLong(DateraUtil.getValue(key, url));
-
-        }catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("Invalid URL format ("+key+" is not an integer)");
-        }
-        return value;
-    }
-
     public static int getReplica(String url)
     {
         return getIntegerValue(DateraUtil.VOLUME_REPLICA,url);
     }
 
-    public static Long getMaxTotalIOPs(String url)
-    {
-        return getLongValue(DateraUtil.MAX_TOTAL_IOPS,url);
-    }
-
-    public static Long getMaxReadIOPs(String url)
-    {
-        return getLongValue(DateraUtil.MAX_READ_IOPS,url);
-    }
-
-    public static Long getMaxWriteIOPs(String url)
-    {
-        return getLongValue(DateraUtil.MAX_WRITE_IOPS,url);
-    }
-
-    public static Long getMaxTotalBandwidth(String url)
-    {
-        return getLongValue(DateraUtil.MAX_TOTAL_BANDWIDTH,url);
-    }
-    public static Long getMaxReadBandwidth(String url)
-    {
-        return getLongValue(DateraUtil.MAX_READ_BANDWIDTH,url);
-    }
-    public static Long getMaxWriteBandwidth(String url)
-    {
-        return getLongValue(DateraUtil.MAX_WRITE_BANDWIDTH,url);
-    }
 
     public static DateraUtil.DateraMetaData getDateraCred(long storagePoolId, StoragePoolDetailsDao storagePoolDetailsDao) {
 
@@ -461,10 +269,6 @@ public class DateraUtil {
         return dtMetaData;
     }
 
-    public static String generateInitiatorLabel(String hostUUID)
-    {
-       return "cs_datera_"+hostUUID;
-    }
     public static String generateAppInstanceName(String storagePoolName, String volumeUUD)
     {
        return storagePoolName+"_"+volumeUUD;
@@ -488,10 +292,10 @@ public class DateraUtil {
 
     public static String generateInitiatorGroupName(String appInstanceName) {
 
-        return "csIG_"+appInstanceName;
+        return "csIG_" + appInstanceName;
     }
     public static String constructInitiatorLabel(String hostUUID)
     {
-        return "cs_"+hostUUID;
+        return "cs_" + hostUUID;
     }
 }
